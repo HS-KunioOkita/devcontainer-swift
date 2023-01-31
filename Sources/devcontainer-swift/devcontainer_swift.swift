9299da9cbee1b5bd3f16459d41b8671aedd13d4a
hs_okita.kunio <okita.kunio@heartsoft.co.jp>
2023-01-31 05:21:14 +0000

    swift 実行環境構築

---
+++ Sources/devcontainer-swift/devcontainer_swift.swift
@@ -0,0 +1,8 @@
+@main
+public struct devcontainer_swift {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(devcontainer_swift().text)
+    }
+}
---
+++ Tests/devcontainer-swiftTests/devcontainer_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import devcontainer_swift
+
+final class devcontainer_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(devcontainer_swift().text, "Hello, World!")
+    }
+}
